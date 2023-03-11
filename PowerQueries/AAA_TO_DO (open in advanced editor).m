be1b81a71c1f90633697b7dbfd8aaf6fb9415fe6
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-03-11 21:14:10 +1100

    Add ToDo as PowerQuery

---
+++ PowerQueries/AAA_TO_DO (open in advanced editor).m
@@ -0,0 +1,10 @@
+/*
+Split all functions
+
+Is adding function metadata going to make debug too cumbersome?
+
+Adding option to consolidate tables and tabs in spreadsheet (including) current spreadsheet so that this file can be complete with examples and the DataSchema
+
+*/
+
+""
